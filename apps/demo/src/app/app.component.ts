488e4373185ad65883bcf3144832ff2f4de683ce
Martin Štumpa <stumpa.martin@gmail.com>
2020-02-26 23:07:52 +0100

    feat(demo): setup basic demo app

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'ngx-cz-tel-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo';
+
+  ctrl = new FormControl('');
+
+  ngOnInit() {
+    this.ctrl.valueChanges.subscribe(val => console.log('appCmp: ', val));
+  }
+
+  slovak() {
+    this.ctrl.setValue('+421800123456');
+  }
+
+  czech() {
+    this.ctrl.setValue('+420800123456');
+  }
+
+  czechShort() {
+    this.ctrl.setValue('800123456');
+  }
+
+  setNull() {
+    this.ctrl.setValue(null);
+  }
 }
--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxCzTelModule } from '@stumpam/ngx-cz-tel';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgxCzTelModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
