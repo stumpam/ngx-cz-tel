c7368d8e943b9ae5f367315ea03e73f6163af58e
Martin Štumpa <stumpa.martin@gmail.com>
2021-06-04 11:16:01 +0200

    build: bump dependencies to Angular 12

--- libs/ngx-cz-tel/src/index.ts
+++ libs/ngx-cz-tel/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/ngx-cz-tel.module';
+export { CzTelDirective } from './lib/directives/cz-tel.directive';
 export { formatCzechPhoneNumber } from './lib/functions/format.functions';
--- libs/ngx-cz-tel/src/lib/ngx-cz-tel.module.spec.ts
+++ libs/ngx-cz-tel/src/lib/ngx-cz-tel.module.spec.ts
@@ -1,8 +1,8 @@
-import { async, TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { NgxCzTelModule } from './ngx-cz-tel.module';
 
 describe('NgxCzTelModule', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [NgxCzTelModule]
     }).compileComponents();
